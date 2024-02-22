dc78be5555f5cf85d1bafd6b883917718e9c3d9b
mojahid-belaman <mojahidbelaman@gmail.com>
2024-02-22 12:31:23 +0100

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/meal_screen.dart';
+
+final myTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color.fromARGB(255, 131, 57, 0),
+      brightness: Brightness.dark),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 void main() {
-  runApp();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: myTheme,
+      home: const MealScreen(
+        title: 'Some Category...',
+        meals: [],
+      ),
+    );
+  }
 }
