8e1978ade36f01ea316585158abbac9696929768
mojahid-belaman <mojahidbelaman@gmail.com>
2024-02-22 12:29:28 +0100

    create widget of cotegory item

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:meals/modals/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).colorScheme.onPrimary,
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              category.color.withOpacity(0.40),
+              category.color.withOpacity(0.90)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            borderRadius: const BorderRadius.all(Radius.circular(10))),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ),
+    );
+  }
+}
