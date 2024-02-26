c0a94595e15131879be4826615873c8c7b24674f
mojahid-belaman <mojahidbelaman@gmail.com>
2024-02-26 15:50:43 +0100

    add icon button to appBar handle toggle favorite meals

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:meals/modals/meal.dart';
+import 'package:meals/widgets/meal_section.dart';
 
 class MealDetailScreen extends StatelessWidget {
-  const MealDetailScreen({super.key, required this.meal});
+  const MealDetailScreen(
+      {super.key, required this.meal, required this.onToggleFavorite});
 
   final Meal meal;
+  final void Function(Meal meal) onToggleFavorite;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
+        actions: [
+          IconButton(
+              onPressed: () {
+                onToggleFavorite(meal);
+              },
+              icon: const Icon(Icons.favorite))
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -24,48 +34,11 @@ class MealDetailScreen extends StatelessWidget {
             const SizedBox(
               height: 15,
             ),
-            Text(
-              'Ingredients',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold),
-            ),
+            MealSection(title: 'Ingredients', description: meal.ingredients),
             const SizedBox(
               height: 15,
             ),
-            for (final ingredient in meal.ingredients)
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                child: Text(
-                  ingredient,
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground),
-                ),
-              ),
-            const SizedBox(
-              height: 15,
-            ),
-            Text(
-              'Steps',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            for (final step in meal.steps)
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                child: Text(
-                  step,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground),
-                ),
-              ),
+            MealSection(title: 'Steps', description: meal.steps),
           ],
         ),
       ),
